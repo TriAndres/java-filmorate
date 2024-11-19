48115ac1ebf50e5ec20e35d665f7bddae31ca6d2
Andrei <An.S-1@yandex.ru>
2024-11-19 22:35:52 +0300

    controllers-films-users

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,61 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.FilmDoesNotExistException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RequestMapping("/films")
 @RestController
 public class FilmController {
+    private final Map<Long, Film> films = new HashMap<>();
+
+    @PostMapping
+    public Film createFilm(@Valid @RequestBody Film film) {
+        log.info("Добавление нового фильма.");
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@Valid @RequestBody Film newFilm) {
+        if (newFilm.getId() == null) {
+            log.error("Не указан id");
+            throw new ValidationException("Id должен быть указан");
+        }
+        if (films.containsKey(newFilm.getId())) {
+            Film oldFilm = films.get(newFilm.getId());
+            log.info("Обновление фильма.");
+            oldFilm.setName(newFilm.getName());
+            oldFilm.setDescription(newFilm.getDescription());
+            oldFilm.setReleaseDate(newFilm.getReleaseDate());
+            oldFilm.setDuration(newFilm.getDuration());
+            return oldFilm;
+        }
+        log.error("Фильм с id = {} не найден", newFilm.getId());
+        throw new FilmDoesNotExistException("Фильм с id = " + newFilm.getId() + " не найден");
+    }
+
+    @GetMapping
+    public Collection<Film> findAllFilm() {
+        log.info("Вывод список фильмов.");
+        return films.values();
+    }
+
+    private Long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }
