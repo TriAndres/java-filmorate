86c8c59a30fb95234ec785802954c507a661468a
Andrei <An.S-1@yandex.ru>
2025-01-11 14:09:43 +0300

    Spring boot template

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,56 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.service.FilmService;
-
-import java.util.Collection;
-import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/films")
-@RequiredArgsConstructor
-@Slf4j
 public class FilmController {
-    private final FilmService filmService;
-
-    @PostMapping
-    public Film createFilm(@Valid @RequestBody Film film) {
-        log.info("Добавление нового фильма.");
-        return filmService.createFilm(film);
-    }
-
-    @PutMapping
-    public Film updateFilm(@Valid @RequestBody Film newFilm) {
-        log.info("Обновление фильма.");
-        return filmService.updateFilm(newFilm);
-    }
-
-    @GetMapping
-    public Collection<Film> findAllFilm() {
-        log.info("Вывод список фильмов.");
-        return filmService.findAllFilm();
-    }
-
-    @PutMapping(value = "/{id}/like/{userId}")
-    public Film addLike(@NotNull @PathVariable long id,
-                        @NotNull @PathVariable long userId) {
-        return filmService.addLike(id, userId);
-    }
-
-    @DeleteMapping(value = "/{id}/like/{userId}")
-    public Film deleteLike(@NotNull @PathVariable long id,
-                           @NotNull @PathVariable long userId) {
-        return filmService.deleteLike(id, userId);
-    }
-
-    @GetMapping("/popular")
-    public List<Film> getPopularFilms(
-            @RequestParam(required = false, defaultValue = "10") int count) {
-        return filmService.getPopular(count);
-    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,41 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
-import lombok.Builder;
-import lombok.Data;
-import ru.yandex.practicum.filmorate.validator.ReleaseDateValidation;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
-
-@Data
-@Builder
+/**
+ * Film.
+ */
+@Getter
+@Setter
 public class Film {
-    private Long id;
-    @NotNull
-    @NotBlank
-    private String name;
-    @NotNull
-    @NotBlank
-    @Size(max = 200)
-    private String description;
-    @NotNull
-    @ReleaseDateValidation
-    private LocalDate releaseDate;
-    @NotNull
-    @Positive
-    private Integer duration;
-    private final Set<Long> likes = new HashSet<>();
-
-    public void addLike(Long id) {
-        likes.add(id);
-    }
-
-    public void deleteLike(Long id) {
-        likes.remove(id);
-    }
 }
